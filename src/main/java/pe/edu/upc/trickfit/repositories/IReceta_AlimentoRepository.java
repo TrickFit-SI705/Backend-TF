09c383ec6cd1552ee981df1f34c5d3febc041969
gilma <gilma.j.baca@gmail.com>
2022-11-21 23:17:06 -0500

    feat: reportes gilma

--- src/main/java/pe/edu/upc/trickfit/repositories/IReceta_AlimentoRepository.java
+++ src/main/java/pe/edu/upc/trickfit/repositories/IReceta_AlimentoRepository.java
@@ -11,7 +11,18 @@ import java.util.List;
 
 @Repository
 public interface IReceta_AlimentoRepository extends JpaRepository<Receta_Alimento, Integer> {
+    @Query("from Receta_Alimento ra where ra.receta.tituloReceta like %:tituloReceta%")
+    List<Receta_Alimento> buscarReceta(@Param("tituloReceta") String tituloReceta);
 
+    @Query("from Receta_Alimento ra where ra.alimento.nombreAlimento like %:nombreAlimento%")
+    List<Receta_Alimento> buscarAlimento(@Param("nombreAlimento") String nombreAlimento);
+
+    @Query(value = "SELECT r.id_receta, r.titulo_receta, SUM(al.kcal_alimento)\n" +
+            "FROM receta r inner JOIN receta_alimento ra ON  r.id_receta = ra.id_receta\n" +
+            "inner JOIN alimento al ON ra.id_alimento = al.id_alimento\n" +
+            "GROUP BY r.id_receta, r.titulo_receta\n" +
+            "having SUM(al.kcal_alimento) <= 500", nativeQuery = true)
+    List<String[]> obtenerCaloriasReceta ();
 
 
 }
